ea22c870176d163a628c46a47971a5c537509408
dhruv <dhruv.160410116084@gmail.com>
2024-06-08 19:20:02 -0400

    RTC connection issue fixed

--- front-end/app/src/Lobby/Lobby.jsx
+++ front-end/app/src/Lobby/Lobby.jsx
@@ -1,182 +1,125 @@
-import { forwardRef, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import UserList from "../Users/UserList";
-import Video from "../Video/Video";
 import { playVideoFromCamera } from "../helper/webrtc";
 import { useLocation } from "react-router-dom";
-import { socket } from "../socket"
+import { socket } from "../socket";
 
-'use strict';
-const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
-let count = 0
-let pc;
-let localStream;
-let remoteSocketId=null;
+const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] };
+let pc = null;
+let localStream = null;
+let remoteSocketId = null;
 
 export default function Lobby(props) {
-  const videoRef = useRef(null)
-  const remoteVideoRef = useRef(null)
-  const [stream, setStream] = useState('');
-
+  const videoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const [stream, setStream] = useState(null);
   const location = useLocation();
 
-
-  function handleVideo() {
-    try {
-
-      playVideoFromCamera().then(data => {
-        // console.log('[handle video() ]stream data', data)
-        setStream(data)
-        // console.log('state: ',stream)
-        videoRef.current.srcObject = data;
-        localStream = data;
-        // setStream(data)
-        return data;
-      })
-      // console.log(videoRef)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  function createConnection() {
-
-    pc = new RTCPeerConnection(configuration);
-    pc.onicecandidate = e => {
-      console.log("Ice candidate: ", e.candidate)
-      if (e.candidate) {
-        socket.emit('candidate', { receiver:remoteSocketId,'candidate': e.candidate });
-      }
-    };
-    pc.ontrack = e => {
-      console.log(e, 'remote track received   ------------->',remoteVideoRef.current.srcObject)
-      remoteVideoRef.current.srcObject = e.streams[0]
-      console.log(e, 'remote track received   ------------->',remoteVideoRef.current.srcObject)
-
-    };
-
-    pc.addEventListener('connectionstatechange', event => {
-      if (pc.connectionState === 'connected') {
-        console.log('Peers connected');
-       
-      }
-    });
-    console.log(stream)
-
-
-    if (stream) {
-      console.log(stream.getTracks())
-      stream.getTracks().forEach(track => pc.addTrack(track,stream))
-    } else {
-      playVideoFromCamera().then(data => {
-        // console.log('[handle video() ]stream data', data)
-        setStream(data)
-        // console.log('state: ',stream)
-        videoRef.current.srcObject = data;
-
-        setStream(data)
-        data.getTracks().forEach(track => pc.addTrack(track,data))
-        return data;
-      })
-    }
-    // console.log('----------pc--------',pc)
-
-
-  }
-
-  function handleStop() {
-    if (stream) {
-      // console.log(videoRef.current.srcObject,videoRef.current.srcObject.getTracks())
-      console.log('inside if candition')
-      console.log(stream.getTracks())
-      stream.getTracks().forEach(t => t.stop())
-      videoRef.current.srcObject = null;
-    }
-  }
-
-
-
-
   useEffect(() => {
-    // debugger
+    handleVideo();
+    setupSocketListeners();
 
+    return () => {
+      cleanup();
+    };
+  }, []);
 
+  const handleVideo = async () => {
+    try {
+      const stream = await playVideoFromCamera();
+      setStream(stream);
+      videoRef.current.srcObject = stream;
+      localStream = stream;
+    } catch (error) {
+      console.error("Error accessing media devices.", error);
+    }
+  };
 
-    handleVideo()
-    console.log('----------------------rerender')
+  const setupSocketListeners = () => {
     socket.emit('user-data', location.state.userName);
 
     socket.on('offer', async message => {
-      // debugger
       console.log("Offer received:", message);
       if (message.offer) {
-        // handleVideo();
-        if (pc) {
-          return
-        }
-
-        await createConnection()
-        await pc.setRemoteDescription(message.offer);
+        await createConnection();
+        await pc.setRemoteDescription(new RTCSessionDescription(message.offer));
         const answer = await pc.createAnswer();
-        socket.emit('answer', { receiver: message.sender, 'answer': answer });
         await pc.setLocalDescription(answer);
+        remoteSocketId=message.sender
+        socket.emit('answer', { receiver: message.sender, answer });
       }
     });
 
     socket.on('answer', async message => {
-      try {
-        console.log("answer received:", message.answer)
-        console.log(pc)
-        if (!pc) {
-          console.error('no peerconnection');
-          return;
-        }
-        console.log(pc.remoteDescription)
-        // if (!pc.remoteDescription) {
-          await pc.setRemoteDescription(message.answer);
-          console.log('remote description set', pc)
-        // }
-      } catch (error) {
-        console.error(error)
+      console.log("Answer received:", message);
+      if (message.answer && pc) {
+        await pc.setRemoteDescription(new RTCSessionDescription(message.answer));
       }
-
-    })
+    });
 
     socket.on('candidate', async message => {
-      console.log('received: ', message.candidate)
-      if (!pc) {
-        console.error('no peerconnection');
-        return;
+      console.log('Candidate received:', message.candidate);
+      if (message.candidate && pc) {
+        try {
+          await pc.addIceCandidate(new RTCIceCandidate(message.candidate));
+        } catch (e) {
+          console.error('Error adding received ICE candidate', e);
+        }
       }
-      // if (!message.candidate) {
-      //   await pc.addIceCandidate(null);
-      // } else {
-        await pc.addIceCandidate(message.candidate);
-      // }
+    });
+  };
 
-    })
+  const createConnection = async () => {
+    if (!pc) {
+      pc = new RTCPeerConnection(configuration);
 
+      pc.onicecandidate = event => {
+        console.log('generate Ice candidate',event.candidate)
+        if (event.candidate) {
+          socket.emit('candidate', { receiver: remoteSocketId   , candidate: event.candidate });
+        }
+      };
 
-    return function cleanup() {
-      if (stream) {
-        stream.getTracks().forEach(t => t.stop());
+      pc.ontrack = event => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = event.streams[0];
+        }
+      };
+
+      pc.onconnectionstatechange = () => {
+        if (pc.connectionState === 'connected') {
+          console.log('Peers connected');
+        }
+      };
+
+      if (localStream) {
+        localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
       }
-      socket.off('offer');
-      socket.off('answer');
-      socket.off('candidate');
-      pc.close()
-    };
-  }, [])
+    }
+  };
 
-
-  async function makeCall(member) {
-    remoteSocketId=member
+  const makeCall = async (member) => {
+    remoteSocketId = member;
     await createConnection();
     const offer = await pc.createOffer();
     await pc.setLocalDescription(offer);
-    console.log(pc)
-    socket.emit('offer', { member, offer })
+    socket.emit('offer', { member, offer });
+  };
 
-  }
+  const cleanup = () => {
+    if (localStream) {
+      localStream.getTracks().forEach(track => track.stop());
+    }
+    if (pc) {
+      pc.close();
+      pc = null;
+    }
+    socket.off('offer');
+    socket.off('answer');
+    socket.off('candidate');
+  };
+
   return (
     <div className="flex flex-col">
       <NavBar userName={location.state.userName} />
@@ -191,6 +134,5 @@ export default function Lobby(props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
--- front-end/app/src/helper/webrtc.js
+++ front-end/app/src/helper/webrtc.js
@@ -1,6 +1,8 @@
  export async function playVideoFromCamera(camera){
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({video:true})
+        const list = await navigator.mediaDevices.enumerateDevices();
+        console.log(list)
+        const stream = await navigator.mediaDevices.getUserMedia({video:{deviceId:'1fa0d17d736afa8f45fc8d2bcb47f0dd5da42d73959edb70f058e061ff7b8d4b'}})
         // console.log(stream)
         return stream
     } catch (error) {
