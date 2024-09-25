640c421c3ea0a1f06435c37c02c58d686429c70d
dhruv <dhruv.160410116084@gmail.com>
2024-09-25 15:23:48 -0400

    socket updated

--- front-end/app/src/socket.js
+++ front-end/app/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 // console.log(import.meta)
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const socket = io("ws://"+window.location.host+":3000");
+export const socket = io("wss://"+window.location.host+":3000");
